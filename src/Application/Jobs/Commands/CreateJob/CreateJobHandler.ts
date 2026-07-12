ae529b394eb90e8b7c9fdda3f298af392263e941
Arnon pedro <arnonpdr@gmail.com>
2026-07-11 21:27:00 -0300

    Comment contracts to test

--- src/Application/Jobs/Commands/CreateJob/CreateJobHandler.ts
+++ src/Application/Jobs/Commands/CreateJob/CreateJobHandler.ts
@@ -2,7 +2,7 @@ import CreateJobCommand from './CreateJobCommand';
 import { IJobRepository } from '../../../../Domain/Repository/IJobRepository';
 import RabbitMqEventBus from '../../../../Infra/EventBus/RabbitMqEventBus';
 import Job from '../../../../Domain/Entity/Job';
-import JobCreatedEvent from '../../../../../../Contracts/Events/JobCreatedEvent';
+// import JobCreatedEvent from '../../../../../../Contracts/Events/JobCreatedEvent';
 import JobRepository from '../../../../Infra/Repository/JobRepository';
 
 export default class CreateJobHandler{
@@ -40,32 +40,32 @@ export default class CreateJobHandler{
 
         this.repository.create(job);
         console.log("PAssou repositorio");
-        var jobCreated = new JobCreatedEvent(
-                        command.Cnpj,
-            command.RazaoSocial,
-            command.EmailRecrutador,
-            command.NomeRecrutador,
-            command.CelularRecrutador,
-            command.TipoVaga,
-            command.Cargo,
-            command.NumeroVagas,
-            command.TipoEstabelecimento,
-            command.DescricaoEmpresa,
-            command.FormaTrabalho,
+//         var jobCreated = new JobCreatedEvent(
+//                         command.Cnpj,
+//             command.RazaoSocial,
+//             command.EmailRecrutador,
+//             command.NomeRecrutador,
+//             command.CelularRecrutador,
+//             command.TipoVaga,
+//             command.Cargo,
+//             command.NumeroVagas,
+//             command.TipoEstabelecimento,
+//             command.DescricaoEmpresa,
+//             command.FormaTrabalho,
 
-            command.Cidade,
-            command.Bairro,
-            command.DescricaoCargo,
-            1,
-            command.CaracteristicasComportamentais,
-            command.FormaRemuneracao,
-            command.Beneficios,            
-            command.JornadaTrabalhoCargo,
-            command.FormaRecebimentoCurriculo,
-            command.EmailRecebimentoCurriculo
-        );
-console.log("Antes do publish");
-        this.eventBus.publish("job.created", jobCreated);
+//             command.Cidade,
+//             command.Bairro,
+//             command.DescricaoCargo,
+//             1,
+//             command.CaracteristicasComportamentais,
+//             command.FormaRemuneracao,
+//             command.Beneficios,            
+//             command.JornadaTrabalhoCargo,
+//             command.FormaRecebimentoCurriculo,
+//             command.EmailRecebimentoCurriculo
+//         );
+// console.log("Antes do publish");
+//         this.eventBus.publish("job.created", jobCreated);
         console.log("depois do publish");
     }
 
